6968088535da3e4e49535a4c1627158978b49880
Naomu Nagasawa <naga.lcw.ld.0203@keio.jp>
2022-04-25 13:12:51 +0900

    7.py refined

--- week1/7.py
+++ week1/7.py
@@ -1,9 +1,7 @@
-import re
-
 pi_str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-pi_str = pi_str.replace(',', '')
-pi_str = pi_str.replace('.', '')
+pi_str = pi_str.replace(',', '')    #, 除去
+pi_str = pi_str.replace('.', '')    #. 除去
 splitted_str = pi_str.split()
-print(splitted_str)
+#print(splitted_str) #単語ごとに分割したリストを表示
 str_len_list = [len(i) for i in splitted_str]
 print(str_len_list)
