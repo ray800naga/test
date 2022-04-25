91e5d17e6a0e947e0cfc5f253d0a83b8011ce257
Naomu Nagasawa <naga.lcw.ld.0203@keio.jp>
2022-04-25 13:13:16 +0900

    7.py saved

--- week1/7.py
+++ week1/7.py
@@ -2,6 +2,6 @@ pi_str = "Now I need a drink, alcoholic of course, after the heavy lectures invo
 pi_str = pi_str.replace(',', '')    #, 除去
 pi_str = pi_str.replace('.', '')    #. 除去
 splitted_str = pi_str.split()
-#print(splitted_str) #単語ごとに分割したリストを表示
+#print(splitted_str)    #単語ごとに分割したリストを表示
 str_len_list = [len(i) for i in splitted_str]
 print(str_len_list)
